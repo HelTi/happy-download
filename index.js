f77325e0956e581b01a0dd51b8c8299bedfce4d4
helti <helti@qq.com>
2019-01-31 16:38:16 +0800

    add:commander.js

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const program = require('commander');
+const program = require('commander')
+const colors = require('colors')
+const download = require('./utils/download')
+const gitClone = require('./utils/gitclone')
 
 program
-  .option('--no-sauce', 'Remove sauce')
+  .version('0.1.0')
+  .usage('[options] <file ...>')
+  .option('-u, --url <url>', 'https or http address', function (url) {
+    let argvs = process.argv.slice(2)
+    if (!url) {
+      console.log('请输入url'.red)
+    } else {
+      let downloadOpt = {}
+      if (!!argvs[2]) {
+        downloadOpt['filename'] = argvs[2]
+      }
+      download(url, downloadOpt)
+    }
+  })
+  .option('-g, --git <url>', 'git repository address', function (val) {
+    console.log('gitaddress', val)
+    gitClone(val, function () {
+
+    })
+  })
   .parse(process.argv);
- 
-console.log(program)  
-console.log('you ordered a pizza');
-if (program.sauce) console.log('  with sauce');
-else console.log(' without sauce');
