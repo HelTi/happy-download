f06183cb5fcc76c1e0a917771c48d98978053c9a
helti <helti@qq.com>
2019-02-03 17:57:49 +0800

    add：添加下载loading效果

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const program = require('commander')
 const colors = require('colors')
+const ora = require('ora');
+
 const download = require('./utils/download')
 const gitClone = require('./utils/gitclone')
+
+const spinner = ora();
+spinner.color = 'yellow';
 
 program
   .version('0.1.0')
@@ -15,13 +20,18 @@ program
       if (!!argvs[2]) {
         downloadOpt['filename'] = argvs[2]
       }
-      download(url, downloadOpt)
+
+      spinner.start('开始下载 \n')
+      download(url, downloadOpt).then((_) => {
+        spinner.succeed('下载完成！')
+      })
     }
   })
   .option('-g, --git <url>', 'git repository address', function (val) {
     console.log('gitaddress', val)
+    spinner.start('开始下载 \n')
     gitClone(val, function () {
-
+      spinner.succeed('下载完成！')
     })
   })
   .parse(process.argv);
--- utils/download.js
+++ utils/download.js
@@ -3,40 +3,49 @@ const fs = require('fs')
 const path = require('path')
 const colors = require('colors')
 const mime = require('mime-types')
+const randomUserAgent = require('./userAgent')
 
 /**
  * url: http地址
  * dirPath： 默认下载文件夹
  * filename: 文件名
  */
-function download(url, { dirPath = 'download', filename = 'file_name' } = {}) {
+function download(url, { dirPath = 'download', filename = 'file_name' } = {}, cb) {
 
   return new Promise((resolve, reject) => {
     const cwd = process.cwd()
     const DOWNLOAD_PATH = path.join(cwd, dirPath)
     if (fs.existsSync(DOWNLOAD_PATH)) {
-      console.log('已存在download文件夹'.green)
+      console.log(`已存在${dirPath}文件夹`.gray)
     } else {
       fs.mkdirSync(DOWNLOAD_PATH)
     }
-    console.log('下载目录'.green, DOWNLOAD_PATH)
-
+    console.log('存放文件夹位置'.gray, DOWNLOAD_PATH)
     //请求下载
     request
-      .get(url)
+      .get({
+        url: url,
+        headers: {
+          'User-Agent': randomUserAgent()
+        }
+      })
       .on('response', (response) => {
         let { headers, statusCode } = response
         let contentType = response.headers['content-type'],
           extension_name = '.' + mime.extension(contentType);
         filename = filename + extension_name
+
         let wr = fs.createWriteStream(path.join(DOWNLOAD_PATH, filename))
         wr.on('finish', function () {
           resolve(true)
+          cb && cb()
         })
-        wr.on('error', function () {
+        wr.on('error', function (err) {
           reject(false)
+          cb && cb(err)
         })
-        request.get(url).pipe(wr)
+        //写入文件
+        response.pipe(wr)
       })
   })
 
